eb14b6e8721ff858a594c739520d85f3f323cc46
yuki <yuqi01.chen@akesobio.com>
2024-05-24 13:39:48 +0800

    fix: update error description of output name exceed 30

--- src/reader/errors.rs
+++ src/reader/errors.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-pub const OUTPUT_NAME_EXCEED: &str = "Output name exceed 30";
+pub const OUTPUT_NAME_EXCEED: &str = "Length of output name exceed 30";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct OutputError {
